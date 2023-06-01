8e781b4dbe1c11ccad9028e41272e23990e4aae0
reedeXx <81296701+reedeXx@users.noreply.github.com>
2023-06-01 22:21:31 +0200

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -31,18 +31,21 @@ def remove_echo(audio ):
     # Aplicar el filtro de cancelación de eco al audio original
 	return np.convolve(audio[:,1], filter_coeffs, mode='same')
 
+#  Definimos el modo de transmisión de audio como 'send'
 mode =  'send'
 name = 'SERVER TRANSMITTING AUDIO'
+#  Capturamos el audio utilizando la función audioCapture y mostramos gráfico.
 audio,context= ps.audioCapture(mode=mode)
-#ps.showPlot(context,name)
+ps.showPlot(context,name)
 
-# Socket Create
+# Creamos un socket del servidor, establecen la dirección IP y el número de puerto
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_ip = '192.168.1.13'
 port = 4980
 backlog = 5
 socket_address = (host_ip,port)
 print('STARTING SERVER AT',socket_address,'...')
+#  Enlazamos el socket a la dirección y se coloca en modo de escucha.
 server_socket.bind(socket_address)
 server_socket.listen(backlog)
 
@@ -54,11 +57,14 @@ while True:
 		while(conti):
 			frame = audio.get()		
 			filtered_frame = remove_echo(frame)
+			# Serializamos el frame de audio filtrado utilizando
 			a = pickle.dumps(filtered_frame )
+			# Empaquetamos el tamaño de los datos serializados utilizando struct.pack
 			message = struct.pack("Q",len(a))+a
+			# Envíamos los datos al cliente a través del socke
 			client_socket.sendall(message)
 
 	else:
 		break
-
+#Se cierra el socket del cliente
 client_socket.close()	
